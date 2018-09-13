a6cbfc2d790360317a56e16173b828f60292ef80
Leopold Joy <leo@lotsa.ca>
2018-09-12 23:03:53 -0400

    adapt Parameterizer for TCRO Registry parameters

--- contracts/Parameterizer.sol
+++ contracts/Parameterizer.sol
@@ -78,47 +78,26 @@ contract Parameterizer {
         token = EIP20Interface(_token);
         voting = PLCRVoting(_plcr);
 
-        // minimum deposit for listing to be whitelisted
-        set("minDeposit", _parameters[0]);
+        // amount of token required to activate bounty / bounty staking limit
+        set("stakingPoolSize", _parameters[0]);
         
         // minimum deposit to propose a reparameterization
         set("pMinDeposit", _parameters[1]);
 
-        // period over which applicants wait to be whitelisted
-        set("applyStageLen", _parameters[2]);
-
         // period over which reparmeterization proposals wait to be processed
-        set("pApplyStageLen", _parameters[3]);
-
-        // length of commit period for voting
-        set("commitStageLen", _parameters[4]);
+        set("pApplyStageLen", _parameters[2]);
         
         // length of commit period for voting in parameterizer
-        set("pCommitStageLen", _parameters[5]);
-        
-        // length of reveal period for voting
-        set("revealStageLen", _parameters[6]);
+        set("pCommitStageLen", _parameters[3]);
 
         // length of reveal period for voting in parameterizer
-        set("pRevealStageLen", _parameters[7]);
-
-        // percentage of losing party's deposit distributed to winning party
-        set("dispensationPct", _parameters[8]);
+        set("pRevealStageLen", _parameters[4]);
 
         // percentage of losing party's deposit distributed to winning party in parameterizer
-        set("pDispensationPct", _parameters[9]);
-
-        // type of majority out of 100 necessary for candidate success
-        set("voteQuorum", _parameters[10]);
+        set("pDispensationPct", _parameters[5]);
 
         // type of majority out of 100 necessary for proposal success in parameterizer
-        set("pVoteQuorum", _parameters[11]);
-
-        // minimum length of time user has to wait to exit the registry 
-        set("exitTimeDelay", _parameters[12]);
-
-        // maximum length of time user can wait to exit the registry
-        set("exitPeriodLen", _parameters[13]);
+        set("pVoteQuorum", _parameters[6]);
     }
 
     // -----------------------
@@ -134,8 +113,7 @@ contract Parameterizer {
         uint deposit = get("pMinDeposit");
         bytes32 propID = keccak256(abi.encodePacked(_name, _value));
 
-        if (keccak256(abi.encodePacked(_name)) == keccak256(abi.encodePacked("dispensationPct")) ||
-            keccak256(abi.encodePacked(_name)) == keccak256(abi.encodePacked("pDispensationPct"))) {
+        if (keccak256(abi.encodePacked(_name)) == keccak256(abi.encodePacked("pDispensationPct"))) {
             require(_value <= 100);
         }
 
@@ -231,7 +209,6 @@ contract Parameterizer {
             revert();
         }
 
-        assert(get("dispensationPct") <= 100);
         assert(get("pDispensationPct") <= 100);
 
         // verify that future proposal appExpiry and processBy times will not overflow
